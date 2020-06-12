7f86570ac83c4215bae08313f0d5aded9fdff742
Guy King <guy@lambertlabs.com>
2020-06-12 17:20:36 +0100

    Profiles view and navigation

--- profiles/models.py
+++ profiles/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Profile
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the profiles index.")
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    print('profile', profile.user.username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
