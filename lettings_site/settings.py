02d840398f3f8da812faf20ba3fad52a142580db
Guy King <guy@lambertlabs.com>
2020-06-12 18:17:01 +0100

    Add lettings app

--- lettings_site/settings.py
+++ lettings_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lettings.apps.LettingsConfig',
     'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
