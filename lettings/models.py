19099a08860628488168b233c786400a7d500dc4
Guy King <guy@lambertlabs.com>
2020-06-14 06:53:02 +0100

    Add ci config

--- lettings/models.py
+++ lettings/models.py
@@ -4,8 +4,8 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 class Address(models.Model):
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
-    street = models.TextField(max_length=64)
-    city = models.TextField(max_length=64)
+    street = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
@@ -15,7 +15,7 @@ class Address(models.Model):
 
 
 class Letting(models.Model):
-    title = models.TextField(max_length=256)
+    title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
